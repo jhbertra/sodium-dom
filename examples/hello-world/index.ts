1f6db3ea718f0ffb3f0e39dc57b377b565aed88f
Jamie Bertram <jamie@pagecloud.com>
2021-04-03 15:43:54 -0400

    add Hello world example

---
+++ examples/hello-world/index.ts
@@ -0,0 +1,27 @@
+import { StreamSink, Unit } from "sodiumjs";
+import { mainWidget } from "../../src/mod";
+
+function repeatEvery(ms: number, f: () => void) {
+  const go = () => {
+    f();
+    setTimeout(go, ms);
+  };
+  setTimeout(go, ms);
+}
+
+mainWidget((builder) => {
+  // Setup state and events
+  // Setup a stream of ticks to go off every 100 ms
+  const sTick = new StreamSink<Unit>();
+  repeatEvery(100, () => sTick.send(Unit.UNIT));
+
+  // Every time the tick occurs, shift the text 1 char to the left
+  const cText = sTick.accum(
+    "__HELLO_REACTIVE_WORLD__",
+    (_, str) => str.slice(1) + str.charAt(0),
+  );
+
+  // Build UI
+  builder.text("Hello, world!");
+  builder.text(cText);
+});
