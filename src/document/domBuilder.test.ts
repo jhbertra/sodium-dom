31dfd419fba14960bbcd9e49f591deea29cb0917
Jamie Bertram <jamie@pagecloud.com>
2021-05-01 21:20:13 -0400

    Rewrite tests as scenarios

--- src/document/domBuilder.test.ts
+++ src/document/domBuilder.test.ts
@@ -1,156 +1,155 @@
 import {
-  runDomBuilderInstruction,
+  runDomBuilderInstructions,
   InsertElement,
   RemoveNode,
-  IncrementCursor,
   InsertText,
+  DomBuilderInstruction,
+  MoveCursor,
 } from "./domBuilder";
 
-describe("runDomBuilderInstruction", () => {
-  describe("InsertElement", () => {
-    it("Appends the correct type of element to an empty document", () => {
-      document.body.innerHTML = "";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = InsertElement("div");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(`"<div></div>"`);
-    });
-    it("Inserts the correct type of element at the cursor position", () => {
-      document.body.innerHTML = "<div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = InsertElement("p");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"<p></p><div></div>"`,
-      );
-    });
-    it("Appends into the correct element", () => {
-      document.body.innerHTML = "<p></p><div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.querySelector("div") as HTMLElement,
-        document: document,
-      };
-      const instruction = InsertElement("p");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"<p></p><div><p></p></div>"`,
-      );
-    });
-  });
-
-  describe("InsertText", () => {
-    it("Appends the text to an empty document", () => {
-      document.body.innerHTML = "";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = InsertText("Hello, world");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(`"Hello, world"`);
-    });
-    it("Inserts the correct type of element at the cursor position", () => {
-      document.body.innerHTML = "<div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = InsertText("Hello, world");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"Hello, world<div></div>"`,
-      );
-    });
-    it("Appends into the correct element", () => {
-      document.body.innerHTML = "<p></p><div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.querySelector("div") as HTMLElement,
-        document: document,
-      };
-      const instruction = InsertText("Hello, world");
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"<p></p><div>Hello, world</div>"`,
-      );
-    });
-  });
-
-  describe("RemoveNode", () => {
-    it("Removes the node at the cursor position", () => {
-      document.body.innerHTML = "<div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = RemoveNode();
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(`""`);
-    });
-    it("Removes from the correct element", () => {
-      document.body.innerHTML =
-        '<p></p><div><p id="I should stay"></p><p/></div>';
-      const context = {
-        cursor: 1,
-        currentParent: document.querySelector("div") as HTMLElement,
-        document: document,
-      };
-      const instruction = RemoveNode();
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"<p></p><div><p id=\\"I should stay\\"></p></div>"`,
-      );
-    });
-    it("Is a no-op if the cursor position is empty", () => {
-      document.body.innerHTML =
-        '<p></p><div><p id="I should stay"></p><p/></div>';
-      const context = {
-        cursor: 2,
-        currentParent: document.querySelector("div") as HTMLElement,
-        document: document,
-      };
-      const instruction = RemoveNode();
-      runDomBuilderInstruction(context, instruction);
-      expect(document.body.innerHTML).toMatchInlineSnapshot(
-        `"<p></p><div><p id=\\"I should stay\\"></p><p></p></div>"`,
-      );
-    });
-  });
-
-  describe("IncrementCursor", () => {
-    it("Increments the cursor", () => {
-      document.body.innerHTML = "<div></div>";
-      const context = {
-        cursor: 0,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = IncrementCursor();
-      runDomBuilderInstruction(context, instruction);
-      expect(context.cursor).toEqual(1);
-    });
-    it("Is a no-op if the cursor is already the maximum", () => {
-      document.body.innerHTML = "<div></div>";
-      const context = {
-        cursor: 1,
-        currentParent: document.body,
-        document: document,
-      };
-      const instruction = IncrementCursor();
-      runDomBuilderInstruction(context, instruction);
-      expect(context.cursor).toEqual(1);
-    });
-  });
+test("InsertElement", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(InsertElement("div"));
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
 });
+
+test("InsertElement; InsertElement", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(InsertElement("div"), InsertElement("p"));
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <p />
+      <div />
+    </body>
+  `);
+});
+
+test("InsertElement; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(InsertElement("div"), RemoveNode());
+  expect(document.body).toMatchInlineSnapshot(`<body />`);
+});
+
+test("InsertElement; Move +1; InsertElement", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(InsertElement("div"), MoveCursor(1), InsertElement("p"));
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <div />
+      <p />
+    </body>
+  `);
+});
+
+test("InsertElement; Move +1; InsertElement; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertElement("div"),
+    MoveCursor(1),
+    InsertElement("p"),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+test("InsertElement; Move +1; InsertElement; Move -1; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertElement("div"),
+    MoveCursor(1),
+    InsertElement("p"),
+    MoveCursor(-1),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <p />
+    </body>
+  `);
+});
+
+test("InsertElement; Move +2; InsertElement; Move -1; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertElement("div"),
+    MoveCursor(2),
+    InsertElement("p"),
+    MoveCursor(-1),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <p />
+    </body>
+  `);
+});
+
+test("InsertElement; Move +1; InsertElement; Move -2; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertElement("div"),
+    MoveCursor(2),
+    InsertElement("p"),
+    MoveCursor(-1),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <p />
+    </body>
+  `);
+});
+
+test("InsertText", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(InsertText("Hello, world"));
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      Hello, world
+    </body>
+  `);
+});
+
+test("InsertElement; Move +1; InsertText; Move -1; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertElement("div"),
+    MoveCursor(1),
+    InsertText("Hello, world"),
+    MoveCursor(-1),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      Hello, world
+    </body>
+  `);
+});
+
+test("InsertText; Move +1; InsertElement; Move -1; RemoveNode", () => {
+  document.body.innerHTML = "";
+  runDomBuilderTest(
+    InsertText("Hello, world"),
+    MoveCursor(1),
+    InsertElement("div"),
+    MoveCursor(-1),
+    RemoveNode(),
+  );
+  expect(document.body).toMatchInlineSnapshot(`
+    <body>
+      <div />
+    </body>
+  `);
+});
+
+function runDomBuilderTest(...instructions: DomBuilderInstruction[]) {
+  runDomBuilderInstructions(document.body, 0, instructions);
+}
--- src/document/domBuilder.ts
+++ src/document/domBuilder.ts
@@ -1,80 +1,106 @@
 import { Tag } from "./core";
 
-/**
- * An instruction to insert an element into the DOM.
- *
- * It will insert a new element at the current index of the child list of the current element.
- * If there is already a node at this index, the new element will be placed before it.
+/*
+ * Instruction types
  */
-export interface InsertElementInstruction {
+
+interface InsertElementInstruction {
   readonly type: "InsertElement";
   readonly tag: Tag;
 }
 
-/**
- * An instruction to insert a text node into the DOM.
- *
- * It will insert the text node at the current index of the child list of the current element.
- * If there is already a node at this index, the new text node will be placed before it.
- */
-export interface InsertTextInstruction {
+interface InsertTextInstruction {
   readonly type: "InsertText";
   readonly content: string;
 }
 
-/**
- * An instruction to remove a node from the DOM.
- *
- * If there is no node at the current index in the current element's child list, nothing will be changed.
- */
-export interface RemoveNodeInstruction {
+interface RemoveNodeInstruction {
   readonly type: "RemoveNode";
 }
 
-/**
- * An instruction to increment the current index by 1.
- *
- * If the current index already equals currentParent.childList.length, this is a no-op.
- */
-export interface IncrementIndexInstruction {
-  readonly type: "IncrementCursor";
+interface MoveCursorInstruction {
+  readonly type: "MoveCursor";
+  readonly delta: number;
 }
 
 /**
  * An instruction to update the DOM builder's state.
  */
 export type DomBuilderInstruction =
-  | IncrementIndexInstruction
+  | MoveCursorInstruction
   | InsertElementInstruction
   | InsertTextInstruction
   | RemoveNodeInstruction;
 
 /**
- * Create a IncrementIndexInstruction
+ * An instruction to move the cursor.
+ *
+ * The cursor is clamped to the range [0, currentParent.childNodes.length].
+ *
+ * Always resets the cursor to single mode.
  */
-export function IncrementCursor(): DomBuilderInstruction {
-  return { type: "IncrementCursor" };
+export function MoveCursor(delta: number): DomBuilderInstruction {
+  return { type: "MoveCursor", delta };
 }
 
 /**
- * Create an InsertElementInstruction
+ * An instruction to insert an element into the DOM.
+ *
+ * It will insert a new element under the cursor of the child list of the current element.
+ * If there is already a node under the cursor, the new element will be placed before it.
+ *
+ * Always resets the cursor to single mode.
  */
 export function InsertElement(tag: Tag): DomBuilderInstruction {
   return { type: "InsertElement", tag };
 }
 
 /**
- * Create an InsertTextInstruction
+ * An instruction to insert a text node into the DOM.
+ *
+ * It will insert the text node under the cursor of the child list of the current element.
+ * If there is already a node under the cursor, the new text node will be placed before it.
+ *
+ * Always resets the cursor to single mode.
  */
 export function InsertText(content: string): DomBuilderInstruction {
   return { type: "InsertText", content };
 }
 
 /**
- * Create a RemoveNodeInstruction
+ * An instruction to remove a node from the DOM.
+ *
+ * If there is no node under the cursor in the current element's child list, nothing will be changed.
+ *
+ * Always resets the cursor to single mode.
  */
 export function RemoveNode(): DomBuilderInstruction {
   return { type: "RemoveNode" };
+}
+
+/*
+ * State types
+ */
+
+export interface CursorSingle {
+  readonly type: "Single";
+  index: number;
+}
+
+export interface CursorSpan {
+  readonly type: "Span";
+  start: number;
+  end: number;
+}
+
+export type Cursor = CursorSingle | CursorSpan;
+
+export function CursorSingle(index: number): Cursor {
+  return { type: "Single", index };
+}
+
+export function CursorSpan(start: number, end: number): Cursor {
+  return { type: "Span", start, end };
 }
 
 /**
@@ -82,9 +108,9 @@ export function RemoveNode(): DomBuilderInstruction {
  */
 export interface DomBuilderContext {
   /**
-   * The index of the current element's child node list currently under focus.
+   * The the child or children currently under focus.
    */
-  cursor: number;
+  cursor: Cursor;
   /**
    * The element whose child nodes are being traversed.
    */
@@ -93,18 +119,42 @@ export interface DomBuilderContext {
    * The document.
    */
   document: Document;
+  /**
+   * A collection of nodes that can be held in reserve to be put elsewhere.
+   */
+  register: Node[];
+}
+
+/*
+ * Instruction handlers
+ */
+
+export function runDomBuilderInstructions(
+  rootElement: HTMLElement,
+  startAt: number,
+  instructions: DomBuilderInstruction[],
+): void {
+  const context: DomBuilderContext = {
+    register: [],
+    document: rootElement.ownerDocument,
+    currentParent: rootElement,
+    cursor: CursorSingle(startAt),
+  };
+  instructions.forEach((instruction) =>
+    runDomBuilderInstruction(context, instruction),
+  );
 }
 
 /**
  * Run an instruction and update the context.
  */
-export function runDomBuilderInstruction(
+function runDomBuilderInstruction(
   context: DomBuilderContext,
   instruction: DomBuilderInstruction,
 ): void {
   switch (instruction.type) {
-    case "IncrementCursor":
-      runIncrementIndex(context);
+    case "MoveCursor":
+      runMoveCursor(context, instruction.delta);
       break;
     case "InsertElement":
       runInsertElementInstruction(context, instruction.tag);
@@ -118,33 +168,62 @@ export function runDomBuilderInstruction(
   }
 }
 
-function runIncrementIndex(context: DomBuilderContext): void {
+function runMoveCursor(context: DomBuilderContext, delta: number): void {
   const { currentParent, cursor } = context;
-  context.cursor = Math.min(currentParent.childNodes.length, cursor + 1);
+  const currentIndex = cursor.type === "Span" ? cursor.start : cursor.index;
+  const newIndex = Math.max(
+    0,
+    Math.min(currentParent.childNodes.length, Math.floor(currentIndex + delta)),
+  );
+  if (cursor.type === "Single") {
+    cursor.index = newIndex;
+  } else {
+    context.cursor = CursorSingle(newIndex);
+  }
 }
 
 function runInsertElementInstruction(
   context: DomBuilderContext,
   tag: Tag,
 ): void {
-  const { document, currentParent, cursor } = context;
+  const { document } = context;
   const element = document.createElement(tag);
-  const nodeAtIndex = currentParent.childNodes[cursor] ?? null;
-  currentParent.insertBefore(element, nodeAtIndex);
+  runInsert(context, element);
 }
 
 function runInsertTextInstruction(
   context: DomBuilderContext,
   content: string,
 ): void {
-  const { document, currentParent, cursor } = context;
+  const { document } = context;
   const text = document.createTextNode(content);
-  const nodeAtIndex = currentParent.childNodes[cursor] ?? null;
-  currentParent.insertBefore(text, nodeAtIndex);
+  runInsert(context, text);
 }
 
 function runRemoveNodeInstruction(context: DomBuilderContext): void {
   const { currentParent, cursor } = context;
-  const nodeAtIndex = currentParent.childNodes[cursor] ?? null;
-  nodeAtIndex?.remove();
+  const newRegister = [];
+  const start = cursor.type === "Single" ? cursor.index : cursor.start;
+  const end = cursor.type === "Single" ? cursor.index : cursor.end;
+  for (let i = start; i <= end; ++i) {
+    const nodeUnderCursor = currentParent.childNodes[i];
+    nodeUnderCursor?.remove();
+    if (nodeUnderCursor) {
+      newRegister.push(nodeUnderCursor);
+    }
+  }
+  if (newRegister.length > 0) {
+    context.register = newRegister;
+  }
+}
+
+function runInsert(context: DomBuilderContext, node: Node): void {
+  const { currentParent, cursor } = context;
+  if (cursor.type === "Single") {
+    const nodeUnderCursor = currentParent.childNodes[cursor.index] ?? null;
+    currentParent.insertBefore(node, nodeUnderCursor);
+  } else {
+    runRemoveNodeInstruction(context);
+    runInsert(context, node);
+  }
 }
